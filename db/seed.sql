46a2a36bf7f81afeeb7d63249d21577d9ccdd7b1
Aricoria10 <empresskitsune10@gmail.com>
2024-02-02 15:46:24 -0500

    adding seed data

--- db/seed.sql
+++ db/seed.sql
@@ -1,15 +1,25 @@
 INSERT INTO department (name)
-VALUES ("Web Development"),
-       ("Data Science"),
-       ("Math"),
-       ("Electives");
+VALUES ("Pharmacy"),
+       ("Health"),
+       ("Law"),
+       ("Education"),
+       ("Programming"),
+       ("Accounting");
 
-INSERT INTO role (name, department)
-VALUES ("Intro to JavaScript", 1),
-       ("Data Science", 2),
-       ("Linear Algebra", 3),
-       ("History of the Internet", 4),
-       ("Machine Learning", 4),
-       ("Game Design", 1 ),
-       ("Cloud Development", 1);
+INSERT INTO position (title, salary)
+VALUES ("Pharmacist", 100000),
+       ("Pharmacy Technician", 35000),
+       ("Nurse", 70000),
+       ("Doctor", 120000),
+       ("Nursing Technician", 25000),
+       ("Lawyer", 140000),
+       ("Legal Assistant", 50000),
+       ("Backend Developer", 80000),
+       ("Frontend Developer", 70000),
+       ("IT Tech", 40000),
+       ("Accountant", 95000),
+       ("Banker", 55000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("Intro to JavaScript", 1);
        

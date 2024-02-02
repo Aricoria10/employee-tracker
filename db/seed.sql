0d7b8763c57b26f48295bcf5336eb57a97e413c1
Aricoria10 <empresskitsune10@gmail.com>
2024-02-02 17:05:41 -0500

    edited config file, was able to drop scheme into sql and added the seed

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO department (name)
+INSERT INTO departments (name)
 VALUES ("Pharmacy"),
        ("Health"),
        ("Law"),
@@ -6,20 +6,33 @@ VALUES ("Pharmacy"),
        ("Programming"),
        ("Accounting");
 
-INSERT INTO position (title, salary)
-VALUES ("Pharmacist", 100000),
-       ("Pharmacy Technician", 35000),
-       ("Nurse", 70000),
-       ("Doctor", 120000),
-       ("Nursing Technician", 25000),
-       ("Lawyer", 140000),
-       ("Legal Assistant", 50000),
-       ("Backend Developer", 80000),
-       ("Frontend Developer", 70000),
-       ("IT Tech", 40000),
-       ("Accountant", 95000),
-       ("Banker", 55000);
+INSERT INTO positions (title, salary, department_id)
+VALUES ("Pharmacist", 100000, 1),
+       ("Pharmacy Technician", 35000, 1),
+       ("Nurse", 70000, 2),
+       ("Doctor", 120000, 2),
+       ("Nursing Technician", 25000, 2),
+       ("Lawyer", 140000. 3),
+       ("Legal Assistant", 50000, 3),
+       ("Backend Developer", 80000, 5),
+       ("Frontend Developer", 70000, 5),
+       ("IT Tech", 40000, 5),
+       ("Accountant", 95000, 6),
+       ("Banker", 55000, 6),
+       ("Professor", 70000, 4),
+       ("Teacher's Assistant", 45000, 4);
 
-INSERT INTO employee (first_name, last_name)
-VALUES ("Intro to JavaScript", 1);
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES  ('John', 'Doe', 1, NULL),
+        ('Jane', 'Smith', 3, 1),
+        ('Bob', 'Johnson', 3, 1),
+        ('Alice', 'Williams', 6, 2),
+        ('Charlie', 'Brown', 3, 1),
+        ('Eva', 'Davis', 6, 2),
+        ('Frank', 'Miller', 3, 1),
+        ('Grace', 'Taylor', 3, 1),
+        ('Harry', 'Wilson', 6, 3),
+        ('Ivy', 'Moore', 3, 1),
+        ('Jack', 'Clark', 3, 1),
+        ('Kelly', 'Anderson', 6, 2);
        
--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 const express = require('express');
-// Import and require mysql2
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
 const sequelize = require('./config/connection');
+const mysql = require('mysql2');
 
-
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to database
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // MySQL password
-    password: '',
-    database: 'courses_db'
-  },
-  console.log(`Connected to the courses_db database.`)
-);
+app.use(express.static('public'));
+
+sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
